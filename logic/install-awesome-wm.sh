f6e02be6b3653e8bc31529c422761e0185dab789
Benjamín de Jesús Pérez Aguilar <mcdreck276@gmail.com>
2022-08-30 18:09:57 -0600

    septima prueva

--- logic/install-awesome-wm.sh
+++ logic/install-awesome-wm.sh
@@ -15,7 +15,7 @@ pkg_navegador="firefox"
 #Manejador de fichero
 pkg_file_manger="thunar"
 #Gestor de escritrorios
-pkg_sddm="sddm"
+pkg_sddm="sddm sddm-kcm"
 #Reproductor de musica en terminal
 pkg_mpd="mpd mpc ncmpcpp"
 #no_confirmar="--noconfirm"
@@ -29,15 +29,28 @@ pkg_herramientas_usuario="${pkg_navegador} ${pkg_mpd}"
 #Variable de instalacion
 instalar_pkg_uno="${sudo} pacman -Syu ${pkg_requisitos} ${pkg_herramientas_escritorio} ${pkg_herramientas_usuario} ${no_confirmar}"
 
+function solicitu_permisos(){
+    clear
+    echo " "
+    echo " "
+    echo " "
+    echo "¡¡¡Permisos!!!"
+    echo " "
+    echo " "
+    echo " "
+    echo " "
+
+}
+
 function instalacion_awesome-wm(){
     ##Instar variable 
-    echo "\n\n\n\n ¡¡Permisos!!!"
+    solicitu_permisos
     ${instalar_pkg_uno}
     ##Escritorio
-    echo "\n\n\n\n ¡¡Permisos!!!"
+    solicitu_permisos
     ${sudo} pacman -Syu ${pkg_awesome} --noconfirm
     ##Gestro de session
-    echo "\n\n\n\n ¡¡Permisos!!!"
+    solicitu_permisos
     ${sudo} pacman -Syu ${pkg_sddm} --noconfirm
 }
 
@@ -49,7 +62,7 @@ function copiar_configuraracion(){
         mkdir -p "$HOME"/.config/awesome
         mkdir -p "$HOME"/.ncmpcpp
         mkdir -p "$HOME"/.mpd
-        mkdir -p /etc/sddm.conf.d/
+        sudo mkdir -p /etc/sddm.conf.d
         echo "Copiando la configuracion"
         cp -r /etc/xdg/awesome/rc.lua "$HOME"/.config/awesome/rc.lua
         cp -r /usr/share/doc/alacritty/example/alacritty.yml "$HOME"/.config/alacritty/alacritty.yml
@@ -58,6 +71,6 @@ function copiar_configuraracion(){
         cp -r configuracionAlacritty/* "$HOME"/.config/alacritty
         cp -r configuracionMpd/* "$HOME"/.mpd
         cp -r configuracionNcmpcpp/* "$HOME"/.ncmpcpp
-        cp -r configuracionSession/* /etc/sddm.conf.d/
+        sudo cp -r configuracionSession/* /etc/sddm.conf.d/
         systemctl enable sddm.service
 }
